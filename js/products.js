db020f13710a8a2b03d24741478f5b1020405f7d
GoYutoPro <91126384+GoYutoPro@users.noreply.github.com>
2021-10-12 10:22:02 +0100

    Add files via upload

---
+++ js/products.js
@@ -0,0 +1,93 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+        Pet01: {
+            type: "Dog Bordeaux",
+            name: "Bobby",
+            breed: "dog de bordeaux",
+            description: "Newly born puppy",
+            img: "dog de bordeaux.jpg",
+            price: 905
+        },
+
+        Pet02: {
+            type: "Dog Shepherds",
+            name: "Alex",
+            breed: "German Shepherd",
+            description: "3 weeks old dog",
+            img: "../images/German Shepherd.jpeg",
+            price: 1200
+        },
+
+        Pet03: {
+            type: "Cat Persian",
+            name: "Melissa",
+            breed: "Persian Cat",
+            description: "8 weeks old cat",
+            img: "../images/catpersian.jpeg",
+            price: 100
+        },
+
+        Pet04: {
+            type: "Cat Norwegian",
+            name: "Fiona",
+            breed: "Norwegian Forest cat",
+            description: "11 weeks old cat",
+            img: "../images/Cat Norwegian.jpeg",
+            price: 110
+
+        },
+
+        Toy01: {
+            name: "Toy Rope",
+            type: "Dogs Rope Toy",
+            description: "Dogs Rope Toy",
+            img: "chicago.jpg",
+            price: 11
+
+        },
+
+        Toy02: {
+            name: "Toy Teaser",
+            type: "the Teaser Toy for Cats",
+            description: "the Teaser Toy for cats",
+            img: "ny.jpg",
+            price: 8
+
+        },
+
+        Toy03: {
+            name: "Cat Wheel Exercise",
+            type: "Cat Wheel toy for cats to exercise",
+            description: "Cat Wheel toy for cats to exercise",
+            img: "ny.jpg",
+            price: 230
+
+        },
+
+
+    }
+    //     123: {
+    //         name: "MokBook Thicc",
+    //         desc: "Greatest properly off ham exercise all.",
+    //         img: "../images/dummy-pdt-b.jpg",
+    //         price: 2034
+    //     },
+    //     124: {
+    //         name: "MokBook Rookie",
+    //         desc: "Unsatiable its possession nor off.",
+    //         img: "../images/la.jpg",
+    //         price: 1247
+    //     },
+    //     125: {
+    //         name: "iPong Max",
+    //         desc: "All difficulty unreserved the solicitude.",
+    //         img: "../images/dummy-pdt-a.jpg",
+    //         price: 675
+    //     },
+    //     126: {
+    //         name: "iTab Pok",
+    //         desc: "Had judgment out property the supplied. ",
+    //         img: "../images/dummy-pdt-a.jpg",
+    //         price: 842
+    //     }
+    // };
